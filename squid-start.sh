20c50071e8d1472d5c56043592acac67c848046e
piragenth-ltfb <linuxtutorialforbeginners@gmail.com>
2023-08-16 13:14:33 +0530

    rebuild

--- squid-start.sh
+++ squid-start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo sysctl -w net.ipv6.conf.eth0.disable_ipv6=0 && sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=0 && sysctl -p && sudo -u proxy -- /usr/local/squid/sbin/squid -d 10
+sudo chown -R proxy:proxy /usr/local/squid
+sudo sysctl -w net.ipv6.conf.eth0.disable_ipv6=0
+sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=0
+sysctl -p 
+sleep 10
+sudo -u proxy -- /usr/local/squid/sbin/squid -d 10 >> squid-log.log
+echo "started" >> squid-start.log
+bash
+# && sleep infinity
